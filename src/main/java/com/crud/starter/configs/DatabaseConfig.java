3003e6dd312fd55ca3d6dd400f0c520a702b0a39
Jeongsik An <ajs15010120@gmail.com>
2023-07-05 10:01:50 +0900

    setting : 자바 프로젝트

---
+++ src/main/java/com/crud/starter/configs/DatabaseConfig.java
@@ -0,0 +1,44 @@
+package com.crud.starter.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        log.info("확인 시작 ======> ");
+        log.info(url);
+        log.info(username);
+        log.info(password);
+        log.info("확인 끝 ======> ");
+
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
+
+}
---
+++ src/test/java/com/crud/starter/StarterApplicationTests.java
@@ -0,0 +1,14 @@
+package com.crud.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test01");
+	}
+
+}
